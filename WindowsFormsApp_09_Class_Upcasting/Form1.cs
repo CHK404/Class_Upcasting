de968bc1fcdff6e9eeb6fceac381b3c056a2ce2c
CHK404 <azbxabcd4@gmail.com>
2025-05-13 09:57:37 +0900

    Add project files.

---
+++ WindowsFormsApp_09_Class_Upcasting/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_09_Class_Upcasting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //1. 자식 객체 생성
+            Sword sword = new Sword();
+            sword.Name = "Long Sword";
+            sword.Attack(); //부모 메서드 호출
+            sword.Sharp();  //자식 고유 메서드 호출
+
+            Console.WriteLine("=====================");
+
+            //2. 업캐스팅: "자식" 클래스를 "부모" 클래스 타입으로 참조
+
+        }
+    }
+}
---
+++ WindowsFormsApp_09_Class_Upcasting/Weapon.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp_09_Class_Upcasting
+{
+    #region 1. 부모 클래스(무기)
+    public class Weapon
+    {
+        //필드
+        //private string name; - 필요 x
+        protected int damage;
+
+        //속성
+        public string Name { get; set; }
+        
+        //생성자
+        public Weapon(int damage)
+        {
+            this.damage = damage;
+        }
+
+        //메서드
+        public void Attack()
+        {
+            Console.WriteLine($"{Name} (으)로 공격");
+        }
+    }
+
+    #endregion
+
+    #region 2. 자식 클래스
+    //자식 클래스: Sword(검)
+    public class Sword : Weapon
+    {
+        //생성자
+        public Sword(int damage) : base(damage) { }
+
+        public void Sharp()
+        {
+            Console.WriteLine("검이 날카롭게 빛납니다.");
+        }
+    }
+    //자식 클래스: Gun(총)
+    public class Gun : Weapon
+    {
+        public void Reload()
+        {
+            Console.WriteLine("총을 재장전 합니다.");
+        }
+    }
+    //자식 클래스: Bow(활)
+    public class Bow : Weapon
+    {
+        public void Aim()
+        {
+            Console.WriteLine("활을 당겨 조준합니다.");
+        }
+    }
+
+    #endregion
+}
